f8cc89668f877e4673bb09b3c2b207eaf80f124f
Ксения Синягина <k.sinyagina@pioneer.ru>
2023-11-14 11:41:14 +0300

    объеденить отдельно точки по каждой комнате

--- Блок 4.1 Выгрузка планировок квартиры/ApartmentLayout/ApartmentLayout/Main.cs
+++ Блок 4.1 Выгрузка планировок квартиры/ApartmentLayout/ApartmentLayout/Main.cs
@@ -36,31 +36,22 @@ namespace ApartmentLayout
                 }
             }
 
-            double MAX_X = 0;
-            double MAX_Y = 0;
+            double MAX_X = -10000000000000000000.0;
+            double MAX_Y = -10000000000000000000.0;
             double MIN_X = 10000000000;
             double MIN_Y = 100000000000;
 
             foreach (var room in rooms)
             {
-                if (MAX_X > room.get_Geometry(new Options()).GetBoundingBox().Max.X)
+                if (MAX_X < room.get_Geometry(new Options()).GetBoundingBox().Max.X)
                     MAX_X = room.get_Geometry(new Options()).GetBoundingBox().Max.X;
-                if (MAX_Y > room.get_Geometry(new Options()).GetBoundingBox().Max.Y)
+                if (MAX_Y < room.get_Geometry(new Options()).GetBoundingBox().Max.Y)
                     MAX_Y = room.get_Geometry(new Options()).GetBoundingBox().Max.Y;
-                if (MIN_X < room.get_Geometry(new Options()).GetBoundingBox().Min.X)
+                if (MIN_X > room.get_Geometry(new Options()).GetBoundingBox().Min.X)
                     MIN_X = room.get_Geometry(new Options()).GetBoundingBox().Min.X;
-                if (MIN_Y < room.get_Geometry(new Options()).GetBoundingBox().Min.Y)
+                if (MIN_Y > room.get_Geometry(new Options()).GetBoundingBox().Min.Y)
                     MIN_Y = room.get_Geometry(new Options()).GetBoundingBox().Min.Y;
             }
-
-
-            /*            var MAX_X = room.get_Geometry(new Options()).GetBoundingBox().Max.X;
-
-                        var MAX_Y = room.get_Geometry(new Options()).GetBoundingBox().Max.Y;
-
-                        var MIN_X = room.get_Geometry(new Options()).GetBoundingBox().Min.X;
-
-                        var MIN_Y = room.get_Geometry(new Options()).GetBoundingBox().Min.Y;*/
 
             List<XYZ> points = new List<XYZ>();
 
@@ -107,32 +98,46 @@ namespace ApartmentLayout
                 }
             }
 
-            if (point_1 != null && point_2 != null && point_3 != null && point_4 != null)
-            {
-                //point 1
-                points.Add(new XYZ(point_4.X, point_1.Y, point_1.Z));
-                //point 2
-                points.Add(new XYZ(point_2.X, point_1.Y, point_1.Z));
-                //point 3
-                points.Add(new XYZ(point_2.X, point_3.Y, point_1.Z));
-                //point 4
-                points.Add(new XYZ(point_4.X, point_3.Y, point_1.Z));
-            }
+            /*            if (point_1 != null && point_2 != null && point_3 != null && point_4 != null)
+                        {
+                            //point 1
+                            points.Add(new XYZ(point_4.X, point_1.Y, point_1.Z));
+                            //point 2
+                            points.Add(new XYZ(point_2.X, point_1.Y, point_1.Z));
+                            //point 3
+                            points.Add(new XYZ(point_2.X, point_3.Y, point_1.Z));
+                            //point 4
+                            points.Add(new XYZ(point_4.X, point_3.Y, point_1.Z));
+                        }*/
+
+            points.Add(new XYZ(0, 0, 0));
+            points.Add(new XYZ(0, 20, 0));
+            points.Add(new XYZ(20, 20, 0));
+            points.Add(new XYZ(20, 10, 0));
+            points.Add(new XYZ(40, 10, 0));
+            points.Add(new XYZ(40, 0, 0));
 
             // Создание объекта цепочки кривых
             CurveLoop loop = new CurveLoop();
 
-            loop.Append(Line.CreateBound(points[0], points[1]));
+/*            loop.Append(Line.CreateBound(points[0], points[1]));
             loop.Append(Line.CreateBound(points[1], points[2]));
             loop.Append(Line.CreateBound(points[2], points[3]));
-            loop.Append(Line.CreateBound(points[3], points[0]));
+            loop.Append(Line.CreateBound(points[3], points[4]));
+            loop.Append(Line.CreateBound(points[4], points[5]));
+            loop.Append(Line.CreateBound(points[5], points[0]));*/
+к
+            for (int i = 1; i < points.Count; i++)
+            {
+                loop.Append(Line.CreateBound(points[i - 1], points[i]));
+                if (i == points.Count - 1)
+                    loop.Append(Line.CreateBound(points[i], points[0]));
+            }
 
             Transaction tr = new Transaction(doc, "view");
             tr.Start();
             // Назначение границ подрезки
             uidoc.ActiveView.GetCropRegionShapeManager().SetCropShape(loop);
-
-            //CropAroundRoom(room, uidoc.ActiveView);
             tr.Commit();
 
             return Result.Succeeded;
